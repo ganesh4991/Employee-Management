4c1af42b00febd9d802003a85f431154b2dc91d9
ganesh4991 <ganesh4991@gmail.com>
2016-05-25 01:25:59 -0400

    Initial Commit
    Creating base employee class and full time employee class

---
+++ employee-management/src/main/java/com/codingchallenge/employeetypes/Employee.java
@@ -0,0 +1,20 @@
+package com.codingchallenge.employeetypes;
+
+import java.util.Date;
+
+public class Employee {
+
+	private int employeeId;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String ssn;
+	private Date dob;
+	private Date employeeJoinDate;
+	private Date employeeEndDate;
+	private String gender;
+	private String address;
+	private String emailId;
+	private String employementType;
+
+}
---
+++ employee-management/src/main/java/com/codingchallenge/employeetypes/FullTimeEmployee.java
@@ -0,0 +1,11 @@
+package com.codingchallenge.employeetypes;
+
+public class FullTimeEmployee extends Employee{
+	private String compensationPlan;
+	private String pay;
+	
+	public FullTimeEmployee() {
+		// TODO Auto-generated constructor stub
+	
+	}
+}
