edf878706c18e4d6acd9a59e549b9ab4147b2e22
ganesh4991 <ganesh4991@gmail.com>
2016-05-25 04:02:00 -0400

    Successful addEmployee application
    Adding getters and setters to the entity class.

--- employee-management/src/main/java/com/codingchallenge/employeetypes/Employee.java
+++ employee-management/src/main/java/com/codingchallenge/employeetypes/Employee.java
@@ -10,6 +10,14 @@ import javax.persistence.Id;
 @Entity
 public class Employee {
 
+	public long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)	
 	private long employeeId;
@@ -28,14 +36,34 @@ public class Employee {
 	private String employementType;
 	*/
 	
-	public Employee(String firstName,String middleName,String lastName){
-		
-		this.firstName=firstName;
-		this.middleName=middleName;
-		this.lastName=lastName;	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Employee(){
 		
 	}
-	
+		
 	@Override
     public String toString() {
         return String.format(
--- employee-management/src/main/java/com/codingchallenge/services/AddEmployeeApplication.java
+++ employee-management/src/main/java/com/codingchallenge/services/AddEmployeeApplication.java
@@ -1,6 +1,12 @@
 package com.codingchallenge.services;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,13 +17,28 @@ import com.codingchallenge.repository.EmployeeRepository;
 
 @RestController
 public class AddEmployeeApplication {
-	
+
 	@Autowired
 	private EmployeeRepository employees;
 	
-	@RequestMapping(value="/employee",method=RequestMethod.POST)
-	public boolean addEmployee(@RequestParam(value="firstName", required=true, defaultValue="") String firstName,@RequestParam(value="middleName", required=false, defaultValue="") String middleName,@RequestParam(value="lastName", required=true, defaultValue="") String lastName){
-		employees.save(new Employee(firstName,middleName,lastName));
-		return false;
+	/*
+	@RequestMapping(value = "/employee", method = RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<Employee>> addEmployee(@RequestParam(value = "firstName", required = true, defaultValue = "") String firstName,
+			@RequestParam(value = "middleName", required = false, defaultValue = "") String middleName,
+			@RequestParam(value = "lastName", required = true, defaultValue = "") String lastName) {
+		employees.save(new Employee(firstName, middleName, lastName));
+		
+		 return new ResponseEntity<Collection<Employee>>(employees.findAll(),
+	                HttpStatus.OK);
+	}
+	*/
+	
+	
+	@RequestMapping(value = "/employee", method = RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+		Employee saved=employees.save(employee);
+		
+		 return new ResponseEntity<Employee> (saved,
+	                HttpStatus.OK);
 	}
 }
