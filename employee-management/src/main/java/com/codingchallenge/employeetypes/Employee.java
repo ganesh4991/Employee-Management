4b6432eeb09f9906108608918f95cbd0a71c6ab7
ganesh4991 <ganesh4991@gmail.com>
2016-05-26 03:17:20 -0400

    Changes reverted

---
+++ employee-management/src/main/java/com/codingchallenge/employeetypes/Employee.java
@@ -0,0 +1,121 @@
+package com.codingchallenge.employeetypes;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="EmployeeMain")
+public class Employee {
+
+	public String getEmploymentType() {
+		return employmentType;
+	}
+
+	public void setEmploymentType(String employmentType) {
+		this.employmentType = employmentType;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)	
+	private long employeeId;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String ssn;
+	private Date dob;
+	private String gender;
+	private String address;
+	private String emailId;
+	private String employmentType;
+	//private EmploymentType employementTypeDetails;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Employee(){
+		
+	}
+		
+	@Override
+    public String toString() {
+        return String.format(
+                "[id=%d, firstName='%s', middleName,lastName='%s']",
+                employeeId, firstName, middleName,lastName);
+    }
+	
+}
